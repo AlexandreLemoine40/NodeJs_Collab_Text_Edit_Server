b036d95a33e5d7369aa9819b65fe43713b65a901
AlexandreLemoine40 <lemoine.alexandre40@gmail.com>
2019-04-13 23:27:27 +0200

    Création d'un serveur NodeJs qui utilise une connexion à un client QuillJS avec Socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./test-quill.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+
+    // Quand un nouveau client se connecte on reçoit son pseudo
+    socket.on('newClient', function(pseudo) {
+        socket.pseudo = pseudo;
+        console.log(pseudo);
+    });
+});
+
+
+
+
+server.listen(8080);
